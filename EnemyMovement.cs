e7d04d444bc0f47f82ba6e812de2e6ad25d190c8
PavlyR <92807832+PavlyR@users.noreply.github.com>
2023-11-10 09:24:47 -0500

    ScriptsWorkInProgress
    Those Scripts are working in progress and actively being worked on.
    
    1. EnemyMovement script involves the enemy movement in the game (at the moment, I am trying to make the enemy move towards the player)
    2. PlayerMovementScript involves the player movement and controls using the Rigidbody physics library in Unity. (At the moment, the player moves, however the player model is not rotating with the camera). There is also a bug with the collision detection.

---
+++ EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float EnemySpeed = 5.0f;
+
+    public GameObject Player;
+    public Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            transform.position = Player.transform.position + transform.forward;
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        transform.Translate(Vector3.forward *  EnemySpeed * Time.deltaTime);
+
+    }
+}
---
+++ PlayerMovementScript.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+
+    [SerializeField] private float walkingSpeed = 5.0f;
+    [SerializeField] private float runningSpeed = 10.0f;
+    [SerializeField] private float jumpSpeed = 10.0f;
+    [SerializeField] private float gravity = 10.0f;
+
+    public Camera playerCamera;
+    public float sensitivity = 5.0f;
+    public float cameraAngle = 45.0f;
+
+    public bool canMove = true;
+    
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement_forward = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerRigidbody.MovePosition(transform.position + movement_forward * Time.deltaTime * walkingSpeed);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+            rotationX = Mathf.Clamp(rotationX, -cameraAngle, cameraAngle);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        }
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        if (isRunning)
+        {
+            playerRigidbody.MovePosition(transform.position + movement_forward * Time.deltaTime * runningSpeed);
+        }
+    }
+}
