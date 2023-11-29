88092a6c6f00178ae4505ba4f95b67b86090e5b5
TamHarris <124204477+TamHarris@users.noreply.github.com>
2023-11-28 20:50:40 -0500

    Scene Change Code Within GameManager
    Have started to put in the code for the scene management, stopping for now due to a migraine but will work on it more tonight or tomorrow.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance;
+
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake()
+    {
+        Instance = this; //Set t
+    }
     void Start()
     {
-        
+        UpdateGameState(GameState.StartMenu)
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         
     }
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState) //This switch state looks at the state of the game and does something based on that.
+        {
+            case GameState.StartMenu:
+                break;
+            case GameState.GameStart:
+                break;
+            case GameState.LoseMenu:
+                break;
+            case GameState.VictoryMenu:
+                break;
+            default:
+
+        }
+        OnGameStateChanged?.(newState);//This checks to see if any scripts care about the event then will send them the info if they do
+    }
 }
+
+public enum GameState
+{
+    StartMenu,
+    GameStart,
+    LoseMenu,
+    VictoryMenu
+
+};
