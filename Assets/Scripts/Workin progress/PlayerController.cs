ae738df347dc5b8fe49818abec1b21e826e5c9bd
PavlyR <92807832+PavlyR@users.noreply.github.com>
2024-01-17 20:29:07 -0500

    Updated PlayerController and PlayerCamera Scripts
    So finally, we are getting rid of the annoying player controller methods that we used in the previous semester and we have a player controller script that relies only on rigidbody. The player camera is also in its own seperate script which gives us more flexibility with the camera script. The code is not commented yet because the code is not done yet, I still have to implement the jumping and dashing. Hopefully those will be done by tomorrow in the afternoon. I also intend to have a white box of the game done by next week.

---
+++ Assets/Scripts/Workin progress/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float speed;
+
+    public Transform direction;
+
+    float horizontal;
+    float vertical;
+
+    Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerInput()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void PlayerMovement()
+    {
+        moveDirection = direction.forward * vertical + direction.right * horizontal;
+        rb.AddForce(moveDirection.normalized * speed * 10f);
+    }
+}
