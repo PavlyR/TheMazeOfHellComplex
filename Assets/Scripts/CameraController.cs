86baf94f7b8f7c5b67afc23ba3d9543ae1804c7b
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-01-25 11:03:01 -0500

    Commented CameraController script
    I have commented the camera code.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float sensitivityX;
-    public float sensitivityY;
+    [SerializeField] public float sensitivityX;                 // This variable stores the sensitivity within the x-axis
+    [SerializeField] public float sensitivityY;                 // This variable stores the sensitivity within the y-axis
     public Transform direction;
-    public float rotationX;
-    public float rotationY;
+    [SerializeField] public float rotationX;                    // This variable stores the angle of the camera within the x-axis
+    [SerializeField] public float rotationY;                    // This variable stores the angle of the camera within the y-axis
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;             // The float variable mouseX stores its values that it takes from the mouse movement and multiplies it with the sensitivity within the x-axis
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;             // The float variable mouseX stores its values that it takes from the mouse movement and multiplies it with the sensitivity within the x-axis
 
-        rotationY += mouseX;
+        rotationY += mouseX;                                                                    
         rotationX -= mouseY;
 
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
-        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);    
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);                                          // Makes sure that the camera can move within the x-asix in all angles
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);                         
     }
 }
