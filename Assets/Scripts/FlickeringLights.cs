d3ef2f3bdea5c26e6231472854ebfe763e1c3ae6
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-03-21 10:52:50 -0400

    Some tweaking with the flickering lights switch
    Still trying to make the flickering sound effect pause when game is paused.

--- Assets/Scripts/FlickeringLights.cs
+++ Assets/Scripts/FlickeringLights.cs
@@ -13,6 +13,8 @@ public class FlickeringLights : MonoBehaviour
     public AudioSource AS;
     public AudioClip LightAudio;
 
+    public bool play;
+
     void Start ()
     {
         _light = GetComponent<Light>();
@@ -20,10 +22,26 @@ public class FlickeringLights : MonoBehaviour
         Timer = Random.Range(MinTime, MaxTime);
     }
 
+    private void Awake()
+    {
+        GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
+    }
+
+    private void GameManagerOnGameStateChanged(GameState state)
+    {
+        if (state == GameState.GameStart)
+        {
+            play = true;
+        }
+        else
+        {
+            play = false;
+        }
+    }
+
     void Update ()
     {
         FlickerLight();
-        
     }
 
     void FlickerLight()
@@ -31,7 +49,7 @@ public class FlickeringLights : MonoBehaviour
         if (Timer > 0)
              Timer -= Time.deltaTime;
 
-        if(Timer<=0)
+        if(Timer <= 0)
         {
          _light.enabled = !_light.enabled;
          Timer = Random.Range(MinTime, MaxTime);
