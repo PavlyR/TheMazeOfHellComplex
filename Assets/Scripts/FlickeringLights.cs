8331cb41608d47ab79c1f8af787ee4b8d19e037b
Izzytj2355 <147166217+Izzytj2355@users.noreply.github.com>
2024-03-21 09:07:35 -0400

    Flickering Lights Script

---
+++ Assets/Scripts/FlickeringLights.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLights : MonoBehaviour
+{
+    public Light _light;
+
+    public float MinTime;
+    public float MaxTime;
+    public float Timer;
+
+    public AudioSource AS;
+    public AudioClip LightAudio;
+
+    void Start ()
+    {
+        Timer = Random.Range(MinTime, MaxTime);
+    }
+
+    void Update ()
+    {
+        FlickerLights();
+        
+    }
+
+    void FlickerLight() 
+    }
+        if (Timer > 0)
+             Timer -= Time.deltaTime;
+
+        if(Timer<=0)
+        {
+         _Light.enabled = !_Light.enabled;
+         Timer = Random.Range(MinTime, MaxTime);
+         AS.PlayOneShot(LightAudio);
+        }
+    }
+
+}
