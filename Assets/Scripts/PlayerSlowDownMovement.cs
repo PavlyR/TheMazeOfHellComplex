c4a345950d743367822f0880672a2fc30b42a49d
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-03-21 15:56:54 -0400

    Shadow People Scripts are work in progress

--- Assets/Scripts/PlayerSlowDownMovement.cs
+++ Assets/Scripts/PlayerSlowDownMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerSlowDownMovement : MonoBehaviour
 {
+    public float moveSpeed;
+
+    [SerializeField] public float moveSlowSpeed = 2.5f;
+
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveSpeed = rb.velocity.magnitude;
     }
 }
