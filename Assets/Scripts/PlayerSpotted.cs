f32c0553c85a121092d00c79d9443f4e01e4a8ab
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-04-01 16:53:50 -0400

    Update (started to work on player spotted script)

--- Assets/Scripts/PlayerSpotted.cs
+++ Assets/Scripts/PlayerSpotted.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PlayerSpotted : MonoBehaviour
 {
  
-    
-    // Start is called before the first frame update
     public void Seen()
     {
         print("I think I have been spotted");
