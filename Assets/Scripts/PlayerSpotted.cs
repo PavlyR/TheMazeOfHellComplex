447f929d4da16f6439ae723e279c5b380e8f8c93
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-04-04 14:27:23 -0400

    Post Processing Works. Implementing effects for when Alan sees you

--- Assets/Scripts/PlayerSpotted.cs
+++ Assets/Scripts/PlayerSpotted.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -10,6 +11,15 @@ public class PlayerSpotted : MonoBehaviour
     [SerializeField] Vignette vignette;
     [SerializeField] public float intensity = 0f;
 
+    [SerializeField] public GameObject backgroundMusicObject;
+    [SerializeField] public AudioSource backgroundMusic;
+    [SerializeField] public GameObject HeartBeatObject;
+    [SerializeField] public AudioSource heartBeat;
+
+    public float pitch = 1.0f;
+    public float transition = 1.75f;
+    public float percentage = 0f;
+
     private void Start()
     {
         volume = GameObject.Find("PlayerCamera").GetComponent<Volume>();
@@ -18,12 +28,16 @@ public class PlayerSpotted : MonoBehaviour
         {
             vignette = temporary;
         }
+        backgroundMusic = backgroundMusicObject.GetComponent<AudioSource>();
+        heartBeat = HeartBeatObject.GetComponent<AudioSource>();
     }
 
     public void Seen()
     {
         print("I think I have been spotted");
         StartCoroutine(StartEffect());
+        speedUpHeartBeat();
+        speedUpBackgroundMusic();
     }
 
     private void Update()
@@ -37,9 +51,22 @@ public class PlayerSpotted : MonoBehaviour
     {
         intensity = 0.5f;
 
-        
+        vignette.intensity.value = intensity;
+        vignette.color.value = Color.red;
 
         yield return intensity;
+    }
+
+    private void speedUpHeartBeat()
+    {
+        heartBeat.pitch = Mathf.Lerp(pitch, pitch, percentage);
+        percentage += Time.deltaTime / transition;
+    }
+
+    private void speedUpBackgroundMusic()
+    {
+        backgroundMusic.pitch = Mathf.Lerp(pitch, pitch, percentage);
+        percentage += Time.deltaTime / transition;
     }
 
 }
