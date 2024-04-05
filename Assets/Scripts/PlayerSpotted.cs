0504ed60a1b435b1cd9972b2b3961ec20b5af454
Pavly Riad <92807832+PavlyR@users.noreply.github.com>
2024-04-04 21:16:26 -0400

    I tried to fix the audio bug for when allen sees you. However I wasn't able to

--- Assets/Scripts/PlayerSpotted.cs
+++ Assets/Scripts/PlayerSpotted.cs
@@ -47,10 +47,6 @@ public class PlayerSpotted : MonoBehaviour
         StartCoroutine(NoEffect());
     }
 
-    private void Update()
-    {
-     
-    }
     private IEnumerator StartEffect()
     {
         intensity = 10f;
